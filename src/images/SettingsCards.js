ae96d23a5c90f223224294fcd039a29689a4ac6d
Thomas Nguyen <imtoshiru@gmail.com>
2017-09-20 17:57:16 -0700

    implemented image icons (temp)

---
+++ src/images/SettingsCards.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class SettingCards extends Component{
+	render(){
+		return(
+			<div className = "card">
+	            <div className = "card_image">
+	              <img src = "${im">
+	            </div>
+	            <div className = "card_name">
+	              ${title}
+	            </div>
+	        </div>
+		);
+	}
+}
